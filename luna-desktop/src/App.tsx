e0a8f7c7c435ca8ab48a71ab28b4c7f2e0e98d56
David Wesst <david@cocobokostudios.com>
2023-10-01 04:45:49 +0000

    added config screen for entering api key

--- luna-desktop/src/App.tsx
+++ luna-desktop/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 import "./App.css";
 
@@ -6,14 +6,18 @@ import ConversationOutput from "./components/conversation-output";
 import ConversationInputForm from "./components/conversation-input-form";
 import Conversation from "./models/conversation";
 
+import { Button, Drawer, FormGroup, InputGroup } from "@blueprintjs/core";
+
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { BaseMessage, HumanMessage } from "langchain/schema";
 
 function App() {
-  const [outputMessage, setOutputMessage] = useState(new Conversation("Output message..."));
+  const [outputMessage, setOutputMessage] = useState(new Conversation("Output message...", "Convo Title"));
+  const [showConfigDialogue, setShowConfigDialogue] = useState(false);
+  const [openaiApiKey, setOpenaiApiKey] = useState("");
 
   const onSendInput = async (s: string) => {
-    const OPENAI_API_KEY = "";
+    const OPENAI_API_KEY = openaiApiKey;
 
     const chatModel = new ChatOpenAI({
       openAIApiKey: OPENAI_API_KEY,
@@ -24,15 +28,33 @@ function App() {
     ];
     const chatResult = await chatModel.predictMessages(messages);
 
-    setOutputMessage(new Conversation(chatResult.content));
+    setOutputMessage(new Conversation(chatResult.content, "Convo Title"));
+  }
+
+  const toggleConfigDialog = () => {
+    setShowConfigDialogue(!showConfigDialogue);
   }
 
   return (
     <section className="container">
+      <header className="convo_header">
+          <h1 className="convo_title">{outputMessage.title}</h1>
+          <Button icon="wrench" onClick={toggleConfigDialog} />
+      </header>
       <ConversationOutput conversation={outputMessage} />
       <ConversationInputForm
         onSend={onSendInput}
         />
+      <Drawer isOpen={showConfigDialogue} onClose={toggleConfigDialog}>
+        <FormGroup
+          label="OpenAI API Key"
+          inline={false}
+          >
+          <InputGroup id="openai_api_key" 
+            placeholder="Enter OpenAI API Key"
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setOpenaiApiKey(e.currentTarget.value)}/>
+        </FormGroup>
+      </Drawer>
     </section>
   );
 }
--- luna-desktop/src/models/conversation.ts
+++ luna-desktop/src/models/conversation.ts
@@ -2,14 +2,16 @@ import ConversationMessage from "./conversation-message";
 
 class Conversation { 
     messageLog: ConversationMessage[];
+    title: string;
 
-    constructor(message?: string) {
+    constructor(message?: string, title: string = "") {
         this.messageLog = [
             {
                 message: message + " is in the message log",
                 user: "test-user"
             }
         ];
+        this.title = title;
     }
 }
 export default Conversation;
