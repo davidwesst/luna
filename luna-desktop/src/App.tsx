7cb83faa279ddd53e947b00889f53c114fc1a061
David Wesst <david@cocobokostudios.com>
2023-10-01 03:29:22 +0000

    making chat request to OpenAI

--- luna-desktop/src/App.tsx
+++ luna-desktop/src/App.tsx
@@ -5,14 +5,26 @@ import "./App.css";
 import ConversationOutput from "./components/conversation-output";
 import ConversationInputForm from "./components/conversation-input-form";
 import Conversation from "./models/conversation";
-import { invoke } from "@tauri-apps/api";
+
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { BaseMessage, HumanMessage } from "langchain/schema";
 
 function App() {
   const [outputMessage, setOutputMessage] = useState(new Conversation("Output message..."));
 
   const onSendInput = async (s: string) => {
-    const response = await invoke("send_message", { "message": s}) as string;
-    setOutputMessage(new Conversation(response));
+    const OPENAI_API_KEY = "";
+
+    const chatModel = new ChatOpenAI({
+      openAIApiKey: OPENAI_API_KEY,
+      temperature: 1.3 
+    });
+    const messages : Array<BaseMessage> = [
+      new HumanMessage({ content: s })
+    ];
+    const chatResult = await chatModel.predictMessages(messages);
+
+    setOutputMessage(new Conversation(chatResult.content));
   }
 
   return (
